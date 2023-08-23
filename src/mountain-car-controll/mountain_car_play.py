dcfec4fd86122400c924385400f48dd65e743c66
artur <arturrzgz@gmail.com>
2023-08-22 22:11:32 -0600

    Code refactor and added doc comments to src/mountain-car-controll/mountain_car_play.py.

--- src/mountain-car-controll/mountain_car_play.py
+++ src/mountain-car-controll/mountain_car_play.py
@@ -15,37 +15,37 @@ import sys
 
 class Car():
     def __init__(self, env):
-        # Initialize the Car class.
-        # Args: 
-        #   env: MountainCar-v0 environment
-
+        """
+        Initialize the Car class.
+         
+        Args: 
+           env: MountainCar-v0 environment
+        """
         # Action size = 3
         # Go left, Go right, Don't move
         self.action_size = env.action_space.n
         print(f'MountainCAr-v0 action size: {self.action_size}')
-
-        # Define actions
-        self.K_LEFT  = 0 # Accelerating to the left: Action(0)
-        self.K_RIGHT = 2 # Accelrating to the right: Action(2)
-        self.K_DOWN  = 1 # No acceleration: Action(1)
+        
+        # Define actions in a key map
+        self.KEY_MAPPING = {
+            pygame.K_LEFT: 0,  # Accelerating to the left: Action(0)
+            pygame.K_RIGHT: 2, # Accelrating to the right: Action(2)
+            pygame.K_DOWN: 1,  # No acceleration: Action(1)
+        }
 
     def get_action(self, pressed_key):
-        # Map pressed key to a corresponding action.
-        # Args:
-        #   pressed_key: Key code of the pressed key
-        # Returns:
-        #   int: Action corresponding to the pressed key
+        """
+         Map pressed key to a corresponding action.
 
-        # Handle the pressed key actions
-        # return the current action mapped to the pressed key
-        if pressed_key == self.K_LEFT:
-            action = self.K_LEFT
-        elif pressed_key == self.K_RIGHT:
-            action = self.K_RIGHT
-        else:
-            action = self.K_DOWN
-        
-        return action
+         Args:
+            pressed_key: Key code of the pressed key
+
+         Returns:
+            int: Action corresponding to the pressed key
+        """
+        # Set default behavior to DOWN button. This way the mountain car won't move if
+        # no signal is detected.
+        return self.KEY_MAPPING.get(pressed_key, self.KEY_MAPPING[pygame.K_DOWN])
         
 # Create the MountainCar-v0 environment
 env = gym.make('MountainCar-v0', render_mode='human')
@@ -66,15 +66,7 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
            # Check for key presses
-           if event.key == pygame.K_LEFT:
-                print('LEFT KEY PRESSED')
-                pressed_key = 0
-           if event.key == pygame.K_RIGHT:
-                print('RIGHT KEY PRESSED')
-                pressed_key = 2
-           if event.key == pygame.K_DOWN:
-                print('DOWN KEY PRESSED')
-                pressed_key = 1
+           pressed_key = event.key
                 
            # Get corresponding action from the Car class
            action = car.get_action(pressed_key)
